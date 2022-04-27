eedf83f49d033df0507610b1c461f2b9daad1124
Jonathan Bridge <jvbridge@gmail.com>
2022-04-27 13:10:54 -0700

    changed variable name

--- routes/diagnostics.js
+++ routes/diagnostics.js
@@ -11,7 +11,30 @@ diagnostics.get('/', (req, res) => {
 
 // POST Route for a error logging
 diagnostics.post('/', (req, res) => {
+  const {isValid, errors} = req.body;
+
+  // If the body is present
+  if (typeof isValid == 'boolean' && errors){
+    // make a new object to append
+    const newDiagnostic = {
+      time: Date.now(),
+      errors,
+      error_id: uuidv4()
+    };
+    // send it in
+    readAndAppend(newDiagnostic, './db/diagnostics');
+    const response = {
+      status: 'success',
+      body: newDiagnostic
+    };
   
+    res.json(response);
+
+  } else {
+    res.json({
+      message:"object is valid"
+    });
+  }
 });
 
 module.exports = diagnostics;
