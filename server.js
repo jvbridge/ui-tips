6eec3893e70985722973b9d8b39e12778be4d2d5
YiLin Ong <chauntelleinc@gmail.com>
2022-04-27 15:18:46 -0400

    added code to server.js and added 404.html

--- server.js
+++ server.js
@@ -27,6 +27,20 @@ app.get('/feedback', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
 );
 
+// This route will handle all the requests that are 
+// not handled by any other route handler. In 
+// this hanlder we will redirect the user to 
+// an error page with NOT FOUND message and status
+// code as 404 (HTTP status code for NOT found)
+app.all('*', (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, '/public/pages/404.html'));
+});
+ 
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
+
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
